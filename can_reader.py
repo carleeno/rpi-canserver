9281bdc12b80da28632d3a47e467512490e764c4
Carlin Hefner <carlinohefner@gmail.com>
2022-02-06 12:09:04 -0500

    improve buffer_usage metric, and better naming

--- can_reader.py
+++ can_reader.py
@@ -2,6 +2,7 @@ import logging
 import os
 from queue import Empty, Full, Queue
 from threading import Thread
+from time import sleep
 
 import can
 import cantools
@@ -14,6 +15,8 @@ class CanReader:
     Messages are also decoded if provided a dbc file.
     """
 
+    buffer_usage: int
+    """Smoothed buffer usage for monitoring performance. Fast rise, slow fall (10/sec)."""
     channel: str
     """The name of the can channel this reader was init'd with."""
     decoded_messages: dict
@@ -30,15 +33,21 @@ class CanReader:
             channel: can0 or can1 (if you have a pican duo)
             dbc_file: optionally provide a filepath to define message decoding
         """
-        self.__bus_name = bus_name
-        self.__can_rx_buffer = Queue(100)
-        self.__can_rx_thread = Thread(target=self.__can_rx_task)
         self.__main_thread = Thread(target=self.__main_task)
         self.__stop = False
         self.channel = channel
+        self.message_queue = Queue(100)
+
+        # Buffer setup
+        self.__can_rx_buffer = Queue(100)
+        self.__buffer_write_thread = Thread(target=self.__write_to_buffer)
+        self.buffer_usage = 0
+        self.__smooth_buffer_usage_thread = Thread(target=self.__smooth_buffer_usage)
+
+        # Decoding setup
+        self.__bus_name = bus_name
         self.decoded_messages = {}
         self.failed_messages = {}
-        self.message_queue = Queue(100)
         if dbc_file:
             self.__db = cantools.db.load_file(dbc_file)
         else:
@@ -49,7 +58,7 @@ class CanReader:
             if bus_name not in buses:
                 raise Exception(f"You must specify bus_name as one of: {buses}")
 
-    def __safe_read(self):
+    def __safe_can_rx(self):
         try:
             message = self.__bus.recv(timeout=1)
         except can.CanError as e:
@@ -58,12 +67,12 @@ class CanReader:
 
         return message
 
-    def __can_rx_task(self):
+    def __write_to_buffer(self):
         while True:
             if self.__stop:
                 return
 
-            message = self.__safe_read()
+            message = self.__safe_can_rx()
             if not message:
                 continue
 
@@ -71,6 +80,16 @@ class CanReader:
                 self.__can_rx_buffer.put(message, block=False)
             except Full:
                 logging.error("Dropping message, buffer is full!")
+
+            self.buffer_usage = max((self.__can_rx_buffer.qsize(), self.buffer_usage))
+
+    def __smooth_buffer_usage(self):
+        while True:
+            if self.buffer_usage > 0:
+                self.buffer_usage += -1
+            if self.__stop:
+                return
+            sleep(0.1)
 
     def __main_task(self):
         while True:
@@ -111,18 +130,14 @@ class CanReader:
         os.system(f"sudo /sbin/ip link set {self.channel} up type can bitrate 500000")
         self.__bus = can.interface.Bus(channel=self.channel, bustype="socketcan_native")
         self.__main_thread.start()
-        self.__can_rx_thread.start()
+        self.__buffer_write_thread.start()
+        self.__smooth_buffer_usage_thread.start()
         logging.info(f"Started can_reader ({self.channel}).")
 
     def stop(self):
-        """This finishes up the buffer and stops. Probably not needed."""
+        """This cleanly stops all threads."""
         logging.info(f"Stopping can_reader ({self.channel})...")
         self.__stop = True
         self.__main_thread.join()
         os.system(f"sudo /sbin/ip link set {self.channel} down")
         logging.info(f"Stopped can_reader ({self.channel}).")
-
-    def buffer_usage(self) -> float:
-        """Get usage of internal buffer in percentage (100 = full)."""
-        buffer_usage = self.__can_rx_buffer.qsize() / self.__can_rx_buffer.maxsize
-        return buffer_usage * 100
