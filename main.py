5c273de2ad73c76ba1cc48d3444dabb058b02ea9
Carlin Hefner <carlinohefner@gmail.com>
2022-02-07 00:20:17 -0500

    ensure exceptions are logged

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ from logging_setup import setup_logging
 
 setup_logging()
 
-if __name__ == "__main__":
+
+def main():
     logging.info("################ CAN-Server is starting ################")
+
     can0 = CanReader(channel="can0", dbc_file=cfg.can0_dbc, bus_name=cfg.can0_bus)
     can0.set_decode_filter(cfg.can0_filter, cfg.can0_filter_exact_match)
     can0.start()
     readers = [can0]
-    
+
     if cfg.pican_duo:
         can1 = CanReader(channel="can1", dbc_file=cfg.can1_dbc, bus_name=cfg.can1_bus)
         can1.set_decode_filter(cfg.can1_filter, cfg.can1_filter_exact_match)
@@ -23,16 +25,21 @@ if __name__ == "__main__":
 
     can_logger = CanLogger(readers)
 
-    while True:
-        try:
+    try:
+        while True:
             sleep(1)
-        except KeyboardInterrupt:
-            logging.warning("Keyboard interrupt detected.")
-            break
-        except SystemExit:
-            logging.warning("System exit requested.")
-            break
+    except KeyboardInterrupt:
+        logging.warning("Keyboard interrupt detected.")
+    except SystemExit:
+        logging.warning("System exit requested.")
 
     can0.stop()
     if cfg.pican_duo:
         can1.stop()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        logging.exception(e)
