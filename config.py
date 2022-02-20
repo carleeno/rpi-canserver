e2b065c3a830fd9a36695bd015a2a614fff01eaa
Carlin Hefner <carlinohefner@gmail.com>
2022-02-20 12:51:29 -0500

    swap default can bus names

--- config.py
+++ config.py
@@ -1,10 +1,25 @@
 # Have fun!
 
 
-can0_bus = "VehicleBus"
+can0_bus = "ChassisBus"
 can0_dbc = "Model3CAN.dbc"
 # Message names as found in the dbc file:
 can0_filter = [
+    "ID04FGPSLatLong",
+    "ID101RCM_inertial1",
+    "ID111RCM_inertial2",
+    "ID155WheelAngles",
+    "ID175WheelSpeed",
+    "ID185ESP_brakeTorque",
+]
+can0_filter_exact_match = True
+
+# If you have a pican DUO:
+pican_duo = True
+can1_bus = "VehicleBus"
+can1_dbc = "Model3CAN.dbc"
+# Message names as found in the dbc file:
+can1_filter = [
     "ID186DIF_torque",
     "ID108DIR_torque",
     "ID2E5FrontInverterPower",
@@ -18,20 +33,5 @@ can0_filter = [
     "ID20CVCRIGHT_hvacRequest",
     "ID273UI_vehicleControl",
     "ID3C2VCLEFT_switchStatus",
-]
-can0_filter_exact_match = True
-
-# If you have a pican DUO:
-pican_duo = True
-can1_bus = "ChassisBus"
-can1_dbc = "Model3CAN.dbc"
-# Message names as found in the dbc file:
-can1_filter = [
-    "ID04FGPSLatLong",
-    "ID101RCM_inertial1",
-    "ID111RCM_inertial2",
-    "ID155WheelAngles",
-    "ID175WheelSpeed",
-    "ID185ESP_brakeTorque",
 ]
 can1_filter_exact_match = True
