87d5d46bf90a387bf7db4701097a91a4f7921b6f
Carlin Hefner <carlinohefner@gmail.com>
2022-02-27 10:50:43 -0500

    move main logic to class

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import logging
-from time import sleep
 
 import config as cfg
 from can_logger import CanLogger
@@ -11,44 +10,68 @@ setup_logging()
 log = logging.getLogger("canserver.main")
 
 
+class CanServer:
+    def __init__(self) -> None:
+        self.can0_reader = CanReader(channel="can0")
+        self.can0_reader.setup_decoding(
+            cfg.can0_dbc, cfg.can0_bus, cfg.can0_filter, cfg.decode_interval
+        )
+        self.can0_logger = CanLogger(self.can0_reader)
+
+        if cfg.pican_duo:
+            self.can1_reader = CanReader(channel="can1")
+            self.can1_reader.setup_decoding(
+                cfg.can1_dbc, cfg.can1_bus, cfg.can1_filter, cfg.decode_interval
+            )
+            self.can1_logger = CanLogger(self.can1_reader)
+
+    def _start_logging(self):
+        self.can0_logger.start_logging()
+        if cfg.pican_duo:
+            self.can1_logger.start_logging()
+
+    def _stop_logging(self):
+        if self.can0_logger.running:
+            self.can0_logger.stop_logging()
+        if cfg.pican_duo:
+            if self.can1_logger.running:
+                self.can1_logger.stop_logging()
+
+    def run(self):
+        self.can0_reader.start_reading()
+        if cfg.pican_duo:
+            self.can1_reader.start_reading()
+
+        # future (blocking) web_server.run() will go here. for now we block on input.
+        while True:
+            command = input("'start' or 'stop' logging: ")
+            if command == "start":
+                self._start_logging()
+            elif command == "stop":
+                self._stop_logging()
+
+    def shutdown(self):
+        self.can0_reader.stop_reading()
+        if cfg.pican_duo:
+            self.can1_reader.stop_reading()
+        self._stop_logging()
+
+
 def main():
     log.info("################ CAN-Server is starting ################")
-
-    can0_reader = CanReader(channel="can0")
-    can0_reader.setup_decoding(
-        cfg.can0_dbc, cfg.can0_bus, cfg.can0_filter, cfg.decode_interval
-    )
-    can0_logger = CanLogger(can0_reader)
-
-    if cfg.pican_duo:
-        can1_reader = CanReader(channel="can1")
-        can1_reader.setup_decoding(
-            cfg.can1_dbc, cfg.can1_bus, cfg.can1_filter, cfg.decode_interval
-        )
-        can1_logger = CanLogger(can1_reader)
-
-    can0_logger.start_logging()
-    can0_reader.start_reading()
-    if cfg.pican_duo:
-        can1_logger.start_logging()
-        can1_reader.start_reading()
-
     try:
-        # future (blocking) web_server.run() will go here. for now we sleep.
-        while True:
-            sleep(10)
+        canserver = CanServer()
+        canserver.run()
     except KeyboardInterrupt:
         log.warning("Keyboard interrupt detected.")
-
-    can0_reader.stop_reading()
-    can0_logger.stop_logging()
-    if cfg.pican_duo:
-        can1_reader.stop_reading()
-        can1_logger.stop_logging()
+        canserver.shutdown()
+    except Exception as e:
+        log.exception(e)
+        try:
+            canserver.shutdown()
+        except:
+            pass
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    except Exception as e:
-        log.exception(e)
+    main()
