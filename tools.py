431524c983ed41552a8ae21477319290a732947f
Carlin Hefner <carlinohefner@gmail.com>
2022-03-30 10:42:54 -0400

    ignore sigint in GracefulKiller

--- tools.py
+++ tools.py
@@ -95,24 +95,20 @@ def deep_update(source, overrides):
             source[key] = value
     return source
 
+
 # source code
 # shamelessly copied from
 # https://stackoverflow.com/a/31464349/2591014
 
 import signal
 
+
 class GracefulKiller:
-  kill_now = False
-  signals = {
-    signal.SIGINT: 'SIGINT',
-    signal.SIGTERM: 'SIGTERM'
-  }
+    kill_now = False
+    signals = {signal.SIGTERM: "SIGTERM"}
 
-  def __init__(self):
-    signal.signal(signal.SIGINT, self.exit_gracefully)
-    signal.signal(signal.SIGTERM, self.exit_gracefully)
+    def __init__(self):
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
 
-  def exit_gracefully(self, signum, frame):
-    print("\nReceived {} signal".format(self.signals[signum]))
-    print("Cleaning up resources. End of the program")
-    self.kill_now = True
+    def exit_gracefully(self, signum, frame):
+        self.kill_now = True
